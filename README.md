3362371068a09feb8685d03d9dec996b5a14a0e4
EdU <EdU@EdUs-iMac.local>
2014-10-08 21:14:18 -0300

    Login con Facebook simple sin estilos

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>Bienvenido a Bycler GitHub project.</h1>
+<ul>Contribuidores
+    <li>Leo</li>
+    <li>EdU</li>
+</ul>
---
+++ server/config.js
@@ -0,0 +1,8 @@
+ServiceConfiguration.configurations.remove({
+  service: "facebook"
+});
+
+ServiceConfiguration.configurations.insert({
+  appId: '1620984164795687',
+  secret: '5ac1e79fd2444feafe5885d4c4b84866'
+});
