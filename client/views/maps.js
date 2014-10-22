397e760512560af47292a7af334049906b42b7b8
zilnus <vsalgado.a@gmail.com>
2014-10-22 01:12:15 -0300

    Fix Merge Reactive Markers

--- client/views/maps.js
+++ client/views/maps.js
@@ -2,61 +2,10 @@ Meteor.subscribe('markers');
 Meteor.subscribe('byclers');
 
 var GoogleMap = function (element) {
-<<<<<<< HEAD
-  var self = this;
-
-  self.element = element;
-  self.markers = { };
-
-  var lat = -36.832383, lng = -73.055458;
-  var mapOptions = {
-    center: new google.maps.LatLng(lat, lng),
-    disableDefaultUI: true,
-    zoom: 17
-  };
-  self.gmap = new google.maps.Map(element, mapOptions);
-  
-  // Seccion de control de marcadores
-  var timeMillis = 0, initialClickPositionMap;
-  
-  google.maps.event.addListener(self.gmap, "mousedown", function (event) {
-    timeMillis = new Date().getTime();
-    initialClickPositionMap = event.latLng;
-  });
-
-  google.maps.event.addListener(self.gmap, "mouseup", function (event) {
-    if(initialClickPositionMap!=null && new Date().getTime() - timeMillis > 1000 
-       && event.latLng.lat==initialClickPositionMap.lat 
-       && event.latLng.lng==initialClickPositionMap.lng) {
-      
-      MarkerEditable = new google.maps.Marker({
-        position: event.latLng,
-        draggable: true, 
-        map: self.gmap,
-        icon: new google.maps.MarkerImage('/imgs/markers/ic_map_peligro.png', null, null, null,
-          new google.maps.Size(48, 48))
-      });
-      $("#markers-menu-wrapper").toggleClass("toggled");
-    }
-    timeMillis = 0;
-  });
-  
-  google.maps.event.addListener(self.gmap, "drag", function(event) {
-    initialClickPositionMap = null;
-  });
-=======
     var self = this;
 
     self.element = element;
-    self.markers = {};
-    self.selectedMarkerId = null,
-
-        self.infowindow = new google.maps.InfoWindow({ content: "" });
-    google.maps.event.addListener(self.infowindow, "closeclick", function () {
-        if (self.selectedMarkerId) {
-            self.selectedMarkerId.set(null);
-        }
-    });
+    self.markers = { };
 
     var lat = -36.832383, lng = -73.055458;
     var mapOptions = {
@@ -65,7 +14,35 @@ var GoogleMap = function (element) {
         zoom: 17
     };
     self.gmap = new google.maps.Map(element, mapOptions);
->>>>>>> 67fdd47f83b0d15824b1f091945b81b0fb04e3f9
+
+    // Seccion de control de marcadores
+    var timeMillis = 0, initialClickPositionMap;
+
+    google.maps.event.addListener(self.gmap, "mousedown", function (event) {
+        timeMillis = new Date().getTime();
+        initialClickPositionMap = event.latLng;
+    });
+
+    google.maps.event.addListener(self.gmap, "mouseup", function (event) {
+        if (initialClickPositionMap != null && new Date().getTime() - timeMillis > 1000
+            && event.latLng.lat == initialClickPositionMap.lat
+            && event.latLng.lng == initialClickPositionMap.lng) {
+
+            MarkerEditable = new google.maps.Marker({
+                position: event.latLng,
+                draggable: true,
+                map: self.gmap,
+                icon: new google.maps.MarkerImage('/imgs/markers/ic_map_peligro.png', null, null, null,
+                    new google.maps.Size(48, 48))
+            });
+            $("#markers-menu-wrapper").toggleClass("toggled");
+        }
+        timeMillis = 0;
+    });
+
+    google.maps.event.addListener(self.gmap, "drag", function (event) {
+        initialClickPositionMap = null;
+    });
 };
 
 // accepts reactive function that returns {lat: Number, lng: Number}
@@ -96,26 +73,26 @@ GoogleMap.prototype.setCenter = function (centerFunc) {
     });
 };
 
-<<<<<<< HEAD
 GoogleMap.prototype.showCurrLocationMarker = function () {
-  var self = this;
-  var latLng = Geolocation.latLng();
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(latLng?latLng.lat:0, latLng?latLng.lng:0),
-    map: self.gmap,
-    icon: new google.maps.MarkerImage('/imgs/markers/my_bycler.png', null, null, null,
-      new google.maps.Size(40, 48))
-  });
-
-  Deps.autorun(function () {
+    var self = this;
     var latLng = Geolocation.latLng();
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(latLng ? latLng.lat : 0, latLng ? latLng.lng : 0),
+        map: self.gmap,
+        icon: new google.maps.MarkerImage('/imgs/markers/my_bycler.png', null, null, null,
+            new google.maps.Size(40, 48))
+    });
 
-    if (latLng) {
-      //marker.setMap(self.gmap);
-      marker.setPosition(new google.maps.LatLng(latLng.lat, latLng.lng));
-    }
-  });
-=======
+    Deps.autorun(function () {
+        var latLng = Geolocation.latLng();
+
+        if (latLng) {
+            //marker.setMap(self.gmap);
+            marker.setPosition(new google.maps.LatLng(latLng.lat, latLng.lng));
+        }
+    });
+};
+
 // accepts minimongo cursor
 // documents must have field marker: {lat: Number, lng: Number, infoWindowContent: String}
 GoogleMap.prototype.setMarkers = function (cursor) {
@@ -133,93 +110,8 @@ GoogleMap.prototype.setMarkers = function (cursor) {
             new google.maps.Size(64, 64))
     });
 
-    /*self.liveQuery = cursor.observe({
-     added: function (doc) {
-     var marker = new google.maps.Marker({
-     position: new google.maps.LatLng(doc.marker.lat, doc.marker.lng),
-     map: self.gmap,
-     ico: new google.maps.MarkerImage('/imgs/markers/robo.png', null, null, null,
-     new google.maps.Size(64, 64))
-     });
-
-     self.markers[doc._id] = marker;
-
-     if (doc.marker.infoWindowContent) {
-     marker.infoWindowContent = doc.marker.infoWindowContent;
-
-     google.maps.event.addListener(marker, "click", function () {
-     self.selectMarker(doc._id);
-     });
-     }
-
-     if (self.selectedMarkerId && self.selectedMarkerId.get() === doc._id) {
-     self.syncWithSelectedMarkerId(doc._id);
-     }
-     },
-     removed: function (doc) {
-     self.markers[doc._id].setMap(null);
-     delete markers[doc._id];
-     },
-     changed: function (doc) {
-     self.markers[doc._id].setPosition(
-     new google.maps.LatLng(doc.marker.lat, doc.marker.lng));
-     marker.infoWindowContent = doc.marker.infoWindowContent;
-     }
-     });*/
 };
 
-
-GoogleMap.prototype.addTestMarkers = function () {
-    var self = this;
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(-36.831407, -73.055569),
-        map: self.gmap,
-        icon: new google.maps.MarkerImage('/imgs/markers/ic_map_servicentro.png', null, null, null,
-            new google.maps.Size(64, 64))
-    });
-
-    marker1 = new google.maps.Marker({
-        position: new google.maps.LatLng(-36.830986, -73.053219),
-        map: self.gmap,
-        icon: new google.maps.MarkerImage('/imgs/markers/ic_mapa_taller.png', null, null, null,
-            new google.maps.Size(64, 64))
-    });
-
-    marker2 = new google.maps.Marker({
-        position: new google.maps.LatLng(-36.831238, -73.055424),
-        map: self.gmap,
-        icon: new google.maps.MarkerImage('/imgs/markers/ic_map_estacionamiento.png', null, null, null,
-            new google.maps.Size(64, 64))
-    });
-
-    marker3 = new google.maps.Marker({
-        position: new google.maps.LatLng(-36.833758, -73.055183),
-        map: self.gmap,
-        icon: new google.maps.MarkerImage('/imgs/markers/ic_map_estacionamiento.png', null, null, null,
-            new google.maps.Size(64, 64))
-    });
-};
-
-GoogleMap.prototype.showCurrLocationMarker = function () {
-    var self = this;
-    var latLng = Geolocation.latLng();
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(latLng ? latLng.lat : 0, latLng ? latLng.lng : 0),
-        map: self.gmap,
-        icon: new google.maps.MarkerImage('/imgs/markers/ic_ladon_marcador.png', null, null, null,
-            new google.maps.Size(64, 64))
-    });
-
-    Deps.autorun(function () {
-        var latLng = Geolocation.latLng();
-
-        if (latLng) {
-            marker.setMap(self.gmap);
-            marker.setPosition(new google.maps.LatLng(latLng.lat, latLng.lng));
-        }
-    });
->>>>>>> 67fdd47f83b0d15824b1f091945b81b0fb04e3f9
-};
 
 // Funcion desactivada por ahora
 GoogleMap.prototype.startAnimation = function () {
@@ -310,45 +202,45 @@ GoogleMap.prototype.setStyle = function (styles) {
 
 // Funcion que agrega una marca
 GoogleMap.prototype.addMarker = function (marker) {
-  var self = this;
-  MarkerEditable = new google.maps.Marker({
-    position: new google.maps.LatLng(marker.position.k, marker.position.B),
-    map: self.gmap,
-    icon: new google.maps.MarkerImage(marker.imgSrc, null, null, null,
-                                      new google.maps.Size(48, 48))
-  });
+    var self = this;
+    MarkerEditable = new google.maps.Marker({
+        position: new google.maps.LatLng(marker.position.k, marker.position.B),
+        map: self.gmap,
+        icon: new google.maps.MarkerImage(marker.imgSrc, null, null, null,
+            new google.maps.Size(48, 48))
+    });
 };
 
 
 // Funcion para inicializar el mapa con los marcadores
 GoogleMap.prototype.init = function () {
-  var self = this;
-  var byclersCounter = 0;
-  var markersCounter = 0;
-  
-  Markers.find({}).observe({
-    added: function(marker) {
-      console.log('Huy, llego marker:' + marker);
-       self.markers[markersCounter++] = new google.maps.Marker({
-        position: new google.maps.LatLng(marker.position.k, marker.position.B),
-        map: self.gmap,
-        icon: new google.maps.MarkerImage(marker.imgSrc, null, null, null,
-          new google.maps.Size(48, 48))
-      });
-    }
-  });
-  
-  Byclers.find({}).observe({
-    added: function(marker) {
-      console.log('Huy, llego marker:' + marker);
-      self.byclers[byclersCounter++] = new google.maps.Marker({
-        position: new google.maps.LatLng(marker.position.k, marker.position.B),
-        map: self.gmap,
-        icon: new google.maps.MarkerImage(marker.imgSrc, null, null, null,
-          new google.maps.Size(48, 48))
-      });
-    }
-  });
+    var self = this;
+    var byclersCounter = 0;
+    var markersCounter = 0;
+
+    Markers.find({}).observe({
+        added: function (marker) {
+            console.log('Huy, llego marker:' + marker);
+            self.markers[markersCounter++] = new google.maps.Marker({
+                position: new google.maps.LatLng(marker.position.k, marker.position.B),
+                map: self.gmap,
+                icon: new google.maps.MarkerImage(marker.imgSrc, null, null, null,
+                    new google.maps.Size(48, 48))
+            });
+        }
+    });
+
+    Byclers.find({}).observe({
+        added: function (marker) {
+            console.log('Huy, llego marker:' + marker);
+            self.byclers[byclersCounter++] = new google.maps.Marker({
+                position: new google.maps.LatLng(marker.position.k, marker.position.B),
+                map: self.gmap,
+                icon: new google.maps.MarkerImage(marker.imgSrc, null, null, null,
+                    new google.maps.Size(48, 48))
+            });
+        }
+    });
 }
 
 Template.googleMap.rendered = function () {
@@ -357,69 +249,12 @@ Template.googleMap.rendered = function () {
     var map = new GoogleMap(template.firstNode);
     var options = template.data;
 
-<<<<<<< HEAD
-  if (options.center) {
-    map.setCenter(options.center);
-  } else if (options.geolocate) {
-    map.showCurrLocationMarker();
-    map.setCenter(Geolocation.latLng);
-  }
-  
-  var byclerStyles = [
-  {
-    featureType: 'road',
-    elementType: 'geometry',
-    stylers: [
-      { color: '#7089d2' },
-      { weight: 1.6 }
-    ]
-  }, {
-    featureType: 'road',
-    elementType: 'labels',
-    stylers: [
-      { saturation: -100 },
-      { invert_lightness: true }
-    ]
-  }, {
-    featureType: 'landscape',
-    elementType: 'geometry',
-    stylers: [
-      { color: '#b7c4d2' }
-    ]
-  }, {
-    featureType: 'poi.school',
-    elementType: 'geometry',
-    stylers: [
-      { hue: '#0066ff' },
-      { lightness: -15 },
-      { saturation: 99 }
-    ]
-  }
-  ];
-  
-  map.setStyle(byclerStyles);
-  map.init();
-};
-=======
     if (options.center) {
         map.setCenter(options.center);
     } else if (options.geolocate) {
         map.showCurrLocationMarker();
         map.setCenter(Geolocation.latLng);
     }
-
-    if (options.selectedMarkerId) {
-        map.bindToSelectedMarkerId(options.selectedMarkerId);
-    }
-
-    /*var marker1 = new google.maps.Marker({
-     position: new google.maps.LatLng(-36.831407, -73.055569),
-     ico: new google.maps.MarkerImage('/imgs/markers/aire.png', null, null, null,
-     new google.maps.Size(64, 64))});
-     marker1.setMap(map);
-
-     options.markers = [ marker1 ]; */
-    map.setMarkers(options.markers);
 
     var byclerStyles = [
         {
@@ -457,8 +292,7 @@ Template.googleMap.rendered = function () {
     ];
 
     map.setStyle(byclerStyles);
-    map.addTestMarkers();
-    map.startAnimation();
+    map.init();
 };
 
 
@@ -488,6 +322,7 @@ if (Meteor.isClient) {
         }
     });
 }
+
 if (Meteor.isCordova) {
     GeolocationBG.config({
         // your server url to send locations to
@@ -516,6 +351,4 @@ if (Meteor.isCordova) {
         // enable this hear sounds for background-geolocation life-cycle.
         debug: false
     });
-}
-/*-------*/
->>>>>>> 67fdd47f83b0d15824b1f091945b81b0fb04e3f9
+};
--- server/config/accounts.js
+++ server/config/accounts.js
@@ -7,14 +7,6 @@ Meteor.startup(function() {
         }]
     });
 
-<<<<<<< HEAD
-  // Add Facebook configuration entry
-  ServiceConfiguration.configurations.insert({
-    "service": "facebook",
-    "appId": "1624038611156909",
-    "secret": "66c008a07a435d9cafc7442300b74f89"
-  });
-=======
     // Add Facebook configuration entry
     ServiceConfiguration.configurations.insert({
         "service": "facebook",
@@ -24,6 +16,5 @@ Meteor.startup(function() {
 
     //Leo Test mobile facebook 304310749756322 352b8ed5f9a2e614b11f9ad0696e8291
 
->>>>>>> 67fdd47f83b0d15824b1f091945b81b0fb04e3f9
 });
 
